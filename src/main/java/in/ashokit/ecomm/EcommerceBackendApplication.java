184307d7054a7075c191efa66df215ce799f3893
Gulvaiz <gulvaizkhan71@gmail.com>
2025-10-20 23:26:30 +0530

    removing unused variables

--- src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
+++ src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
@@ -8,14 +8,7 @@ public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
-		int i=10;
-		int j=10;
-		String name="Ashokit";
-		int k=20;
 
-		int k=10;
-		boolean status=false;
-		int a=10;
 	}
 
 }
