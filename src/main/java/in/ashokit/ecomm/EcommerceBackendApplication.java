c6f5571d18ba1067a31cdbb787d01241b7493929
Gulvaiz <gulvaizkhan71@gmail.com>
2025-10-21 14:58:11 +0530

    JIRA 102 is completed

--- src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
+++ src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceBackendApplication {
+	
+	//JIRA -102
+	pulic void m2(){
+		//logic
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
